27d8fd2f5eab902e559b18c4945687b6dd92301e
zzl0 <zhuzhaolong0@gmail.com>
2022-08-31 15:45:19 -0400

    add orst

---
+++ code/orst/src/lib.rs
@@ -0,0 +1,30 @@
+pub trait Sorter {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: Ord;
+}
+
+pub fn sort<T, S>(slice: &mut [T])
+where
+    T: Ord,
+    S: Sorter,
+{
+    S::sort(slice)
+}
+
+mod bubblesort;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
